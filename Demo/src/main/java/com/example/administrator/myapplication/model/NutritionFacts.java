1b841d478b103f60ec8e13338f8a18da55f131a4
liujingyuan <906514731@qq.com>
2015-12-14 23:09:44 +0800

    增加构建者模式

---
+++ Demo/src/main/java/com/example/administrator/myapplication/model/NutritionFacts.java
@@ -0,0 +1,38 @@
+package com.example.administrator.myapplication.model;
+
+/**
+ * Created by Administrator on 2015/12/14.
+ * 建造者模式
+ */
+public class NutritionFacts {
+    private int fat;
+    private int sodium;
+    private int serviceSize;
+
+    public NutritionFacts(Builder builder) {
+        fat=builder.fat;
+        serviceSize=builder.serviceSize;
+        sodium=builder.sodium;
+    }
+
+    public static class Builder {
+        private int fat = 0;
+        private int sodium = 0;
+        private int serviceSize = 0;
+
+        public Builder(int serviceSize, int sodium) {
+            this.serviceSize = serviceSize;
+            this.sodium = sodium;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public NutritionFacts Builder() {
+            return  new NutritionFacts(this);
+        }
+
+    }
+}
